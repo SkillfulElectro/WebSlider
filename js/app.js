a92d00c0b8c3d14c584255bebe4282f40900098c
ElectroMutex <97535877+SkillfulElectro@users.noreply.github.com>
2026-01-06 12:54:39 +0330

    Delete js directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    Renderer.render();
-});
-
-window.addEventListener('resize', () => {
-    if (AppState.currentPage === 'slider') {
-        Renderer.render();
-    }
-});
